2b12c235495d2f366226fcf322828601fa627704
Anusha-Pwr <anusha00466@gmail.com>
2023-06-19 10:47:14 +0530

    changed routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,14 +16,14 @@ function Navbar() {
             <div className="leftSide" id={openLinks ? "open" : "close"}>
                 <img src={logo} />
                 <div className="hiddenLinks">
-                    <Link to="/">Home</Link>
+                    <Link to="/Pizza-app">Home</Link>
                     <Link to="/menu">Menu</Link>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Contact</Link>
                 </div>
             </div>
             <div className="rightSide">
-                <Link to="/">Home</Link>
+                <Link to="/Pizza-app">Home</Link>
                 <Link to="/menu">Menu</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
